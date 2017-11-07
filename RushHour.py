1658c92f524ebfeef0711f5ef66a3f6047e35ada
Iris Schlundt Bodien <irisschlundtbodien@wcw-stud-145-109-48-90.wireless.uva.nl>
2017-11-07 15:49:52 +0100

    x-board

--- RushHour.py
+++ RushHour.py
@@ -7,8 +7,8 @@ def main():
 
     for i in range(6):
         for j in range(6):
-            print("  x  ", end='');
-        print('\n');
+            print("  x  ", end='')
+        print('\n')
 
 if __name__ == "__main__":
     main()
