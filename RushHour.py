7a169838937e5d8f09846db54c3e45de545e2f5c
Eloy Testerink <eloytesterink@wcw-eduroam-195-169-175-132.wireless.uva.nl>
2017-11-08 15:58:43 +0100

    move car

--- RushHour.py
+++ RushHour.py
@@ -1,5 +1,6 @@
 import sys
-from python import RED
+# from python import RED
+# import numpy as np
 
 
 def main():
@@ -8,36 +9,55 @@ def main():
 # Create a grid
     width, height = 6, 6;
     #Matrix = [["X" for x in range(width)] for y in range(height)]
-
+    a = 2
+    b = 1
+    c = 2
     grid_x=[]
+    red = ""
+
+    def RED():
+        grid_x[a][b:(b+2)] = ['#', '#']
+        red = grid_x[a][b:(b+2)]
+        print (grid_x[a][b:c])
+
 
     def moveLeft():
-        a = 2
-        b = 0
-        grid_x[a][b] = '#'
-        b -= 1
+        grid_x[a][b:(c+1)] = ['x', 'x']
+        grid_x[a][(b+1):(c+2)] = ['#', '#']
 
-    for x in range(height):
-        grid_y=[]
-        for y in range (width):
-            #
-            grid_y.append('x')
-        grid_x.append(grid_y)
+    def hasWon():
+        if red == grid_x[2][4:5]:
+            print("you have won")
 
-    print(grid_x[2][0:2])
-    grid_x[2][0:1] = ['#', '#']
+    grid_x = [["x" for x in range(6)]for y in range(6)]
+    # for x in range(height):
+    #     grid_y=[]
+    #     for y in range (width):
+    #         #
+    #         grid_y.append(' ')
+    #     grid_x.append(grid_y)
+
+    RED()
+
+    moveLeft()
+    moveLeft()
+    moveLeft()
+    print(grid_x)
+    hasWon()
 
 
 
-    #print(RED.position_x_R)
     print(grid_x)
 
-    for i in range(height):
-        for j in range (width):
-            print("  ", end='')
-            print(grid_x[i][j], end='')
-            print("  ", end='')
-        print('\n')
+    #print(RED.position_x_R)
+
+
+    # for i in range(height):
+    #     for j in range (width):
+    #         print("  ", end='')
+    #         print(grid_x[i][j], end='')
+    #         print("  ", end='')
+    #     print('\n')
 
 if __name__ == "__main__":
     main()
