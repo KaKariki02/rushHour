072a0d0bce16474ab9f9258daed2803a7493af3b
romanlakerveld <31651999+romanlakerveld@users.noreply.github.com>
2017-11-14 16:17:44 +0100

    classes gemaakt voor board en vehicles: - board opzetten en laden van een file

--- RushHour.py
+++ RushHour.py
@@ -1,60 +1,54 @@
 import sys
-<<<<<<< HEAD
 import numpy as np
+import csv
 
-class gameboard(object):
-    def __init__(self):
-        self.board = [[0 for x in range(6)] for y in range(6)]
+CAR_IDS = ['#','A', 'B', 'C', 'D', 'E']
+TRUCK_IDS = ['O','P','Q','R','S','T','U']
 
-    def printboard(self):
-        print(np.matrix(self.board))
-
-    def  putcar(self):
-        self.board[1][1] = "A"
-        self.board[2][1] = "A"
-=======
-from python import RED
-
-
-def main():
+class Vehicle():
+    def __init__(self, id, x, y, orientation):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.orientation = orientation
+        if id in CAR_IDS:
+            self.length = 2
+        if id in TRUCK_IDS:
+            self.length = 3
+class Gameboard():
 #GRID = 6
 
-# Create a grid
-    width, height = 6, 6;
-    #Matrix = [["X" for x in range(width)] for y in range(height)]
+    def __init__(self):
+        width, height = 6, 6;
+        #Matrix = [["X" for x in range(width)] for y in range(height)]
 
-    grid_x=[]
+        self.board = [["." for x in range(width)] for y in range(height)]
 
-    def moveLeft():
-        a = 2
-        b = 0
-        grid_x[a][b] = '#'
-        b -= 1
+    def printboard(self):
+        print("New board:")
+        print('\n\n'.join(['      '.join(['{}'.format(item) for item in row]) for row in self.board]))
 
-    for x in range(height):
-        grid_y=[]
-        for y in range (width):
-            #
-            grid_y.append('x')
-        grid_x.append(grid_y)
+    def setupBoard(self):
+        for vehicle in self.vehicles:
+            if vehicle.orientation == 'H':
+                for i in range(vehicle.length):
+                    self.board[int(vehicle.y)][int(vehicle.x)+i] = vehicle.id
+            if vehicle.orientation == 'V':
+                for i in range(vehicle.length):
+                    self.board[int(vehicle.y)+i][int(vehicle.x)] = vehicle.id
 
-    print(grid_x[2][0:2])
-    grid_x[2][0:1] = ['#', '#']
-
+    def checkformoves(self):
 
 
-    #print(RED.position_x_R)
-    print(grid_x)
+    def uploadBoard(self):
+        self.vehicles = []
+        with open('game1length2.csv', 'r') as csvboard:
+            boardreader = csv.DictReader(csvboard)
+            for row in boardreader:
+                self.vehicles.append(Vehicle(row['id'], row['x'], row['y'], row['orientation']))
 
-    for i in range(height):
-        for j in range (width):
-            print("  ", end='')
-            print(grid_x[i][j], end='')
-            print("  ", end='')
-        print('\n')
->>>>>>> c2d1b265ccda7eb7b486f1c4917f9876694e8cdd
-
-board = gameboard()
-board.printboard()
-board.putcar()
-board.printboard()
+p = Gameboard()
+p.printboard()
+p.uploadBoard()
+p.setupBoard()
+p.printboard()
